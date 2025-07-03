f007376e7cce3fc2f0d591f85ded255bcbc9d518
JPhy45 <pernia795@gmail.com>
2025-07-03 14:35:23 -0400

    solucionado problema con el descarche

--- SupervisorBravo.WorkerService/Worker.cs
+++ SupervisorBravo.WorkerService/Worker.cs
@@ -20,7 +20,7 @@ namespace SupervisorBravo.WorkerService
         const ushort CONTROL_ON_OFFXR = 512;
         const ushort CONTROL_ON_OFFXT_WRITE = 1280;
         const ushort CONROL_ON_OFFXT_READ = 2049;
-        const ushort THAWING_XR = 513;
+        const ushort THAWING_XR = 535;
 
         public Worker(ILogger<Worker> logger, IServiceScopeFactory scopeFactory)
         {
